ebbdf12cd35db99c9cb9b4ab83f7669e81d95a09
K Sreenivasa Reddy <ksreenivasareddy@Ks-MacBook-Air.local>
2017-06-20 21:38:01 +0530

    Commit

--- app/src/server.js
+++ app/src/server.js
@@ -6,6 +6,10 @@ app.get('/', function (req, res) {
     res.send("Hello Hasura!");
 });
 
+app.get('/contact/', function (req, res) {
+    res.send("Contact US!");
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
