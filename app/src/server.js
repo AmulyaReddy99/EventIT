02e51c3dd16083aee9b61f2ec98a00377e41dcb1
K Sreenivasa Reddy <ksreenivasareddy@Ks-MacBook-Air.local>
2017-06-21 16:20:29 +0530

    Commit1

--- app/src/server.js
+++ app/src/server.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
 //your routes here
 app.get('/', function (req, res) {
-    res.send("Hello Hasura!");
+    res.render('index');
 });
 
 app.get('/contact/', function (req, res) {
     res.send("Contact US!");
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!');
 });
