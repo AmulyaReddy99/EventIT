86c341d6a41912101cf3e25263cb17684a855e8d
K Sreenivasa Reddy <ksreenivasareddy@Ks-MacBook-Air.local>
2017-06-22 09:51:08 +0530

    New

--- app/src/server.js
+++ app/src/server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.set('views', './src/views');
-app.set('view engine', 'ejs')
-app.use(express.static(__dirname + '/public'))
+// app.set('views', './src/views');
+// app.set('view engine', 'ejs')
+// app.use(express.static(__dirname + '/public'))
 
 //your routes here
 app.get('/', function (req, res) {
 	// res.send("Hello Hasura!");
-	res.render('index')
+	res.sendFile(__dirname + '/public/index.html')
 });
 
 app.get('/contact/', function (req, res) {
