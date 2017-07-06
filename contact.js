b7a993426997ba526256798b692a3fd484dadc0e
AmulyaReddy99 <amulyareddyk97@gmail.com>
2017-07-06 13:03:51 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,6 @@
+window.onload=function(){
+	document.getElementById('submit_btn').onclick=function() {
+		alert('sent');
+	};
+	document.getElementById('test').innerHTML=document.getElementById('name').toString;
+};
