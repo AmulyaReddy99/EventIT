576e3e674c5fa7663ec4c84fc00196d53e5c622c
K Sreenivasa Reddy <ksreenivasareddy@Ks-MacBook-Air.local>
2017-08-24 20:07:57 +0530

    deleted extra files

--- contact.js
+++
@@ -1,6 +0,0 @@
-window.onload=function(){
-	document.getElementById('submit_btn').onclick=function() {
-		alert('sent');
-	};
-	document.getElementById('test').innerHTML=document.getElementById('name').toString;
-};
